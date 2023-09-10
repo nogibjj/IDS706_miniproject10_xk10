13f29a54e957f475b42f29e39709b16cab60c1c7
cassiekang <143849077+cassiekang@users.noreply.github.com>
2023-09-10 19:22:18 +0000

    update

--- test_main.py
+++ test_main.py
@@ -3,23 +3,25 @@ Test goes here
 
 """
 
-from main import add
+import os
+import main
 
+def test_read_dataset():
+    # Check if the dataset file exists
+    dataset_file = "bmi.csv"  # Update with your dataset file name
+    assert os.path.exists(dataset_file)
 
-def test_add_positive_numbers():
-    assert add(3, 5) == 8
+def test_generate_summary_and_visualization():
+    # Read the dataset
+    dataset_file = "bmi.csv"  # Update with your dataset file name
+    data = main.read_dataset(dataset_file)
 
-
-def test_add_negative_numbers():
-    assert add(-3, -5) == -8
-
-
-def test_add_mixed_numbers():
-    assert add(3, -5) == -2
-
+    # Check if summary statistics are calculated correctly
+    summary_stats = main.generate_summary_and_visualization(data, 'Age', 'histogram.png', 'summary_report.md')
+    
+    # You can add additional assertions here to check the generated summary statistics or visualization if needed.
 
 if __name__ == "__main__":
-    test_add_positive_numbers()
-    test_add_negative_numbers()
-    test_add_mixed_numbers()
-    print("All tests passed.")
+    test_read_dataset()
+    test_generate_summary_and_visualization()
+
