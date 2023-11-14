8506148f4b097dbd0bcf8e9cedd23ef87a6f3fb8
cassiekang <143849077+cassiekang@users.noreply.github.com>
2023-11-14 03:22:21 +0000

    update

--- main.py
+++ main.py
@@ -1,47 +1,39 @@
 """
-Main code
+Main
 """
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+
+from mylib.lib import (
+    extract,
+    load_data,
+    describe,
+    query,
+    example_transform,
+    start_spark,
+    end_spark,
+)
 
 
-def readfile(file_path):
-    data = pd.read_csv(file_path)
-    return data
-
-
-# Function to generate summary statistics and create a data visualization
-def generate_summary_and_visualization(
-    data, output_summary_report, histogram_image_path
-):
-    # Generate summary statistics for numeric columns
-    summary_stats = data.describe()
-
-    # Create a histogram visualization for the 'Age' column
-    plt.figure(figsize=(8, 6))
-    sns.histplot(data["Age"], kde=True, color="skyblue")
-    plt.title("Age Distribution")
-    plt.xlabel("Age")
-    plt.ylabel("Frequency")
-    plt.savefig(histogram_image_path, bbox_inches="tight")
-    plt.close()
-
-    # Save the summary report in Markdown format
-    with open(output_summary_report, "w", encoding="utf-8") as markdown_file:
-        markdown_file.write(summary_stats.to_markdown())
-        markdown_file.write(f"\n![Histogram]({histogram_image_path})\n")
-    print(f"Summary report saved to {output_summary_report}")
+def main():
+    # extract data
+    extract()
+    # start spark session
+    spark = start_spark("BIRTH")
+    # load data into dataframe
+    df = load_data(spark)
+    # example metrics
+    describe(df)
+    # query
+    query(
+        spark,
+        df,
+        "SELECT YEAR, Month, Births FROM BIRTHS where Births>8000",
+        "BIRTHS",
+    )
+    # example transform
+    example_transform(df)
+    # end spark session
+    end_spark(spark)
 
 
 if __name__ == "__main__":
-    # Specify the input CSV file and output file paths
-    input_file_path = "bmi.csv"  # Update with your dataset file path
-    summary_report = "summary_report.md"
-    histogram_image = "age_histogram.png"
-
-    # Read the dataset
-    df = readfile(input_file_path)
-
-    # Generate summary statistics and create a histogram
-    generate_summary_and_visualization(df, summary_report, histogram_image)
+    main()
--- test_main.py
+++ test_main.py
@@ -1,26 +1,66 @@
 """
-Test goes here
-
+Test 
 """
+import os
+import pytest
+from mylib.lib import (
+    extract,
+    load_data,
+    describe,
+    query,
+    example_transform,
+    start_spark,
+    end_spark,
+)
 
-import main  # Import your main module
-import pandas as pd  # Import pandas
+
+@pytest.fixture(scope="module")
+def spark():
+    spark = start_spark("TestApp")
+    yield spark
+    end_spark(spark)
 
 
-def test_generate_summary_and_visualization():
-    """Test the generate_summary_and_visualization function in main.py"""
-    input_file_path = "bmi.csv"
-    output_summary_report = "test_summary_report.md"
-    histogram_image_path = "test_age_histogram.png"
+def test_extract():
+    file_path = extract()
+    assert os.path.exists(file_path) is True
 
-    # Read the dataset using pandas
-    data = pd.read_csv(input_file_path)
 
-    # Call the function from main.py with the DataFrame
-    main.generate_summary_and_visualization(
-        data, output_summary_report, histogram_image_path
+def test_load_data(spark):
+    df = load_data(spark)
+    assert df is not None
+
+
+def test_describe(spark):
+    df = load_data(spark)
+    result = describe(df)
+    assert result is None
+
+
+def test_query(spark):
+    df = load_data(spark)
+    result = query(
+        spark,
+        df,
+        "SELECT * FROM BIRTH8000 WHERE Births > 8000",
+        "BIRTH8000"
+        # call the "Spark" session
+        # Call the dataframe called "df"
+        # The new dataframe is called "BIRTH8000"
+        # query the data based on the new dataframe
     )
+    assert result is None
+
+
+def test_example_transform(spark):
+    df = load_data(spark)
+    result = example_transform(df)
+    assert result is None
 
 
 if __name__ == "__main__":
-    test_generate_summary_and_visualization()
+    test_extract()
+    test_load_data(spark)
+    test_describe(spark)
+    test_query(spark)
+    test_example_transform(spark)
